84066f80444f96ef7f2a6dce2f281b8bd921a7bc
Azer Chalabiyev <azer.chelebiyev@gmail.com>
2023-06-27 03:16:53 +0400

    3

--- AWS/aws-group-membership.tf
+++ AWS/aws-group-membership.tf
@@ -2,16 +2,21 @@ resource "aws_iam_group_membership" "team" {
   name = "tf-testing-group-membership"
 
   users = [
-    aws_iam_user.team.name,
+    aws_iam_user.devops1.name,
+    aws_iam_user.devops2.name,
   ]
 
-  group = aws_iam_group.testgroup.name
+  group = aws_iam_group.devops.name
 }
 
 resource "aws_iam_group" "group" {
-  name = "testgroup"
+  name = "devops"
 }
 
 resource "aws_iam_user" "user_one" {
-  name = "testuser"
+  name = "devops1"
+}
+
+resource "aws_iam_user" "user_two" {
+  name = "devops2"
 }
