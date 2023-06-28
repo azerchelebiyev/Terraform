c12d7240546860a99aa9f39a13efffc1a31d7cfe
Azer Chalabiyev <azer.chelebiyev@gmail.com>
2023-06-28 22:36:05 +0400

    Used local values

--- aws-create-two_ec2/aws-create-two_ec2.tf
+++ aws-create-two_ec2/aws-create-two_ec2.tf
@@ -1,19 +1,18 @@
 resource "aws_instance" "web" {
-  ami = "ami-0c0e36522a91d66e1"
+  ami           = "ami-0c0e36522a91d66e1"
   instance_type = "t3.micro"
-
-  tags = {
-    Department = "finance"
-    Project = "cerberus"
-  }
+  tags          = local.common_tags
 }
 
 resource "aws_instance" "db" {
-  ami = "ami-0c0e36522a91d66e1"
+  ami           = "ami-0c0e36522a91d66e1"
   instance_type = "t3.micro"
+  tags          = local.common_tags
+}
 
-  tags = {
+locals {
+  common_tags = {
     Department = "finance"
-    Project = "cerberus"
+    Project    = "cerberus"
   }
 }
